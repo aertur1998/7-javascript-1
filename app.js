e971fb88c9185134ed9fe1c97c4ef69de943743a
aertur1998 <150601485+aertur1998@users.noreply.github.com>
2023-11-12 19:57:35 +0100

    Merge remote-tracking branch 'origin/master'

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-console.log('Hello World!')
+//console.log('Hello World!')
+/*let a = 1;
+console.log(a);
+a = 'test'
+console.log(a);
+*/
+
+//Базовые арифметические операции
+/*const width = 10;
+const height = 5;
+const space = width * height;
+console.log(space)
+const newWidth = width - 4;
+console.log(newWidth)
+const newWidth2 = width + 4;
+console.log(newWidth2)
+const division = newWidth / newWidth2;
+console.log(division)
+const volume = 2 ** 3;
+console.log(volume)
+
+//Строки
+const city = 'Gdynia';
+const street = 'Pawia';
+console.log(city + ', ' + street + ' ' + 6);
+*/
+
+/*
+//Операторы присваивания
+let age = 18 + 5;
+age += 2;
+console.log(age);
+age -= 2;
+console.log(age);
+age *= 2;
+console.log(age);
+age /= 4;
+console.log(age);
+
+//Операторы Сравнения
+const vasia = 20;
+console.log(age > vasia);
+console.log(age >= vasia);
+console.log(age < vasia);
+console.log(age <= vasia);
+console.log(age == vasia);
+*/
